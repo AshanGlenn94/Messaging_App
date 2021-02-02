1854d4704cd0b8f6134e2322952df9da4d824745
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-01 20:08:17 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="49cae20e-0900-4ced-a586-54c41c4ac04b"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
